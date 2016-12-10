386b94350bbb4aa1f732a552d8174565c33ff2a7
adamjawer <ajawer@me.com>
2016-12-09 22:37:12 -0500

    Added Core Data Stack and Managed Object Model file

---
+++ TZea/Library/CoreDataStack/CoreDataStack.swift
@@ -0,0 +1,51 @@
+//
+//  CoreDataStack.swift
+//  TZea
+//
+//  Created by Adam Jawer on 12/9/16.
+//  Copyright © 2016 Adam Jawer. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack: NSObject {
+    static let moduleName = "TZea"
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelUrl = Bundle.main.url(forResource: moduleName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelUrl)!
+    }()
+    
+    lazy var applicationDocumentsDirectory: URL? = {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        let persistentStoreUrl = self.applicationDocumentsDirectory?.appendingPathComponent("\(moduleName).sqlite")
+        
+        do {
+            try coordinator.addPersistentStore(
+                ofType: NSSQLiteStoreType,
+                configurationName: nil,
+                at: persistentStoreUrl,
+                options: [NSMigratePersistentStoresAutomaticallyOption: true,
+                          NSInferMappingModelAutomaticallyOption: false])
+        } catch {
+            fatalError("Persistent store error! \(error)")
+        }
+
+        return coordinator
+    }()
+
+    lazy var managedObjectContext: NSManagedObjectContext = {
+       let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        
+        return managedObjectContext
+    }()
+
+}
